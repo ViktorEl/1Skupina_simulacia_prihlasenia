fa5e0f729115b1840799bef6bcb08cbf2e1136f7
Your Name <you@example.com>
2025-04-11 12:51:54 +0200

    formular

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Najskôr sa musíte prihlásiť</h2>
+
+    <form action="index.php" method="post">
+        <input type="email" name="mail" placeholder="Zadajte email" required> <br>
+        <input type="password" name="heslo" placeholder="Zadajte heslo" required> <br>
+        <input type="submit" name="tlacidlo" value="Prihlásiť">
+    </form>
+
+
+
+    
+</body>
+</html>
